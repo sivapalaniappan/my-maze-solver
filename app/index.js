e174f259f93554f1bd20a05c7080e13b01cd9acc
sivapalaniappan <sivawillbe@ymail.com>
2017-11-20 20:10:25 -0600

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div><h1>Hello World</h1></div>, document.getElementById('root'));
